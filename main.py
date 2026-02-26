69cf73d8c3535d92330df1e003e3907ce7834255
ameer <ameer@gmail.com>
2026-02-26 04:23:48 +0200

    added answer_router

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
 from controller.question_controller import router as question_router
+from controller.answer_controller import router as answer_router
 
 from repository.database import database
 
 app = FastAPI()
 app.include_router(question_router)
+app.include_router(answer_router)
 
 @app.on_event("startup")
 async def startup():
